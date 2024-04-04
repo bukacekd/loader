ee60ab7c15e68d2c5a72e676b47eb82840f34622
bukacekd <bukacek.david@gmail.com>
2024-04-04 20:04:57 +0200

    fix namespace collision for iife bundle

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,16 +1,29 @@
 import esbuild from 'esbuild';
-import {readFile} from 'node:fs/promises';
+import {readFile, writeFile} from 'node:fs/promises';
 
-const TemplateMinifyPlugin = {
-    name: "TemplateMinifyPlugin",
+const OptimizePlugin = {
+    name: 'OptimizePlugin',
     setup: build => {
         build.onLoad({filter: /.ts/}, async args => {
             const content = await readFile(args.path, 'utf8');
-            
+
             return {
                 contents: content.replace(/\s\s+/g, ''),
                 loader: 'ts'
             }
+        });
+
+        build.onEnd(async () => {
+            let content = await readFile(build.initialOptions.outfile, 'utf8');
+
+            if (build.initialOptions.format === 'iife') {
+                content = content.replace(
+                    new RegExp(`([^.]+\\.${build.initialOptions.globalName.split('.').pop()}=)(.+);`),
+                    `$1Object.assign(${build.initialOptions.globalName}??{},$2);`
+                );
+            }
+
+            await writeFile(build.initialOptions.outfile, content);
         });
     }
 };
@@ -19,9 +32,10 @@ await esbuild.build({
     entryPoints: ['src/index.ts'],
     format: 'esm',
     logLevel: 'info',
+    metafile: true,
     minify: true,
     outfile: 'dist/index.mjs',
-    plugins: [TemplateMinifyPlugin],
+    plugins: [OptimizePlugin],
 });
 
 await esbuild.build({
@@ -29,7 +43,8 @@ await esbuild.build({
     format: 'iife',
     globalName: 'js4y.components',
     logLevel: 'info',
+    metafile: true,
     minify: true,
     outfile: 'dist/index.js',
-    plugins: [TemplateMinifyPlugin]
+    plugins: [OptimizePlugin],
 });
